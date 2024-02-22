6b006c141f97ebf30867d0c7216ba46403795777
Courtney Letsa <courtneyletsa@gmail.com>
2024-02-22 11:11:52 +0000

    serializer models added successfully

--- main/admin.py
+++ main/admin.py
@@ -6,13 +6,13 @@ class CourseAdmin(admin.ModelAdmin):
     list_display=("name", "date_created", "date_modified")
     
 class ClassScheduleAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "start_date_and_time", "end_date_and_time", "is_repeated", "repeat_frequency", "is_active", "organizer", "cohort", "venue")
+    list_display = ("title", "description", "start_date_and_time", "end_date_and_time", "is_repeated", "repeat_frequency", "meeting_types","is_active", "organizer", "cohort", "course","facilitator","venue", "date_created", "date_modified")
     
 class ClassAttendanceAdmin(admin.ModelAdmin):
     list_display = ("class_schedule", "attendee", "is_present", "date_created", "date_modified", "author")
     
-class ResolutionStatusAdmin(admin.ModelAdmin):
-    list_display=("resolution_status",)
+# class ResolutionStatusAdmin(admin.ModelAdmin):
+#     list_display=("resolution_status",)
 
 class QueryAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "submitted_by", "assigned_to", "resolution_status", "date_created", "date_modified", "author")
@@ -24,6 +24,6 @@ class QueryCommentAdmin(admin.ModelAdmin):
 admin.site.register(Course, CourseAdmin)
 admin.site.register(ClassSchedule, ClassScheduleAdmin)
 admin.site.register(ClassAttendance, ClassAttendanceAdmin)
-admin.site.register(ResolutionStatus, ResolutionStatusAdmin)
+# admin.site.register(ResolutionStatus, ResolutionStatusAdmin)
 admin.site.register(Query, QueryAdmin)
 admin.site.register(QueryComment, QueryCommentAdmin)
--- main/models.py
+++ main/models.py
@@ -15,16 +15,31 @@ class Course(models.Model):
         
     
 class ClassSchedule(models.Model):
+    MEETING_TYPES = (
+        ("CLASS_SECTIONS", 'Class Sessions'),
+        ("WELLNESS_SESSIONS", 'Well Session'),
+        ('GUEST_LECTURE', 'Guest Lecture')
+    )
+    REPEAT_FREQUENCY = (
+        ("DAILY", 'Daily'),
+        ("WEEKLY", 'Weekly'),
+        ('MONTHLY', 'Monthly')
+    )
     title = models.CharField(max_length = 255)
     description = models.TextField(default ="N/A", blank=True, null=True)
     start_date_and_time = models.DateTimeField()
     end_date_and_time = models.DateTimeField()
     is_repeated = models.BooleanField(default = False)
-    repeat_frequency = models.IntegerField()
+    repeat_frequency = models.CharField(max_length=20, choices=REPEAT_FREQUENCY, default='DAILY')
+    meeting_types = models.CharField(max_length=20, choices=MEETING_TYPES, default='CLASS SESSIONS')
     is_active = models.BooleanField(default = False)
     organizer = models.CharField(max_length = 255)
     cohort = models.ForeignKey(Cohort, on_delete = models.CASCADE)
+    course = models.ForeignKey(Course, on_delete = models.SET_NULL, blank=True, null=True, related_name="course_class_schedule")
+    facilitator = models.ForeignKey(IMUser, on_delete = models.SET_NULL, blank=True, null=True, related_name="facilitator_class_schedule")
     venue = models.CharField(max_length = 255)
+    date_created = models.DateTimeField(auto_now_add = True, blank = True, null = True)
+    date_modified = models.DateTimeField(auto_now = True, blank = True, null=True)
     
     def __str__(self):
         return self.title
@@ -42,18 +57,30 @@ class ClassAttendance(models.Model):
     #     return f"(self.member.attendee) in (self.title)"
     
     
-class ResolutionStatus(models.Model):
-    resolution_status = models.CharField(max_length = 225, unique = True)
+# class ResolutionStatus(models.Model):
+#     resolution_status = models.CharField(max_length = 225, unique = True)
     
-    def __str__(self):
-        return self.resolution_status
+#     def __str__(self):
+#         return self.resolution_status
     
 class Query(models.Model):
+    PENDING = 'PENDING'
+    IN_PROGRESS = 'IN_PROGRESS'
+    DECLINED = 'DECLINED'
+    RESOLVED = 'RESOLVED'
+    RESOLUTION_STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (IN_PROGRESS, 'In Progress'),
+        (DECLINED, 'Declined'),
+        (RESOLVED, 'Resolved'),
+    ]
+
     title = models.CharField(max_length = 255)
     description = models.TextField(default ="N/A", blank=True, null=True)
     submitted_by = models.ForeignKey(IMUser, on_delete = models.CASCADE)
     assigned_to = models.ForeignKey(IMUser, on_delete = models.CASCADE, related_name = 'assigned')
-    resolution_status = models.ForeignKey(ResolutionStatus, on_delete = models.CASCADE)
+    # resolution_status = models.ForeignKey(ResolutionStatus, on_delete = models.CASCADE)
+    resolution_status = models.CharField(max_length = 50, choices = RESOLUTION_STATUS_CHOICES, default=PENDING)
     date_created = models.DateTimeField(auto_now_add = True)
     date_modified = models.DateTimeField(auto_now = True)
     author = models.ForeignKey(IMUser, on_delete = models.CASCADE, related_name = 'author')
